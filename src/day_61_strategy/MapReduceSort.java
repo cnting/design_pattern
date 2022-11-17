0947a5031ea1218fde06a63403464e2bc8819b38
cnting <1040594303@qq.com>
2022-11-17 11:09:01 +0800

    策略模式

---
+++ src/day_61_strategy/MapReduceSort.java
@@ -0,0 +1,8 @@
+package day_61_strategy;
+
+public class MapReduceSort implements ISortAlg{
+    @Override
+    public void sort(String filePath) {
+
+    }
+}
---
+++ src/day_61_strategy/QuickSort.java
@@ -0,0 +1,9 @@
+package day_61_strategy;
+
+public class QuickSort implements ISortAlg {
+
+    @Override
+    public void sort(String filePath) {
+
+    }
+}
